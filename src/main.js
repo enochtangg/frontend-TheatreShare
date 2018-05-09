28be3b3b3b88e5a58de454339f07a51eb9592e28
enochtangg <enochtangg@gmail.com>
2018-05-09 17:13:22 -0400

    minor changes

--- src/main.js
+++ src/main.js
@@ -20,32 +20,6 @@ Vue.use(VueYouTubeEmbed);
 
 Vue.config.productionTip = false;
 
-// Initial connection to backend websocket
-// Correctly decide between ws:// and wss://
-// let ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-// let ws_path = ws_scheme + '://localhost:8000' + "/chat/stream/";
-// console.log("Connecting to " + ws_path);
-// const ReconnectingWebSocket = require('reconnecting-websocket');
-// window.socket = new ReconnectingWebSocket(ws_path);
-
-// // Helpful debugging
-// window.socket.onopen = function () {
-//   console.log("Connected to chat socket");
-// };
-// window.socket.onclose = function () {
-//   console.log("Disconnected from chat socket");
-// };
-//
-// window.socket.onmessage = function (message) {
-//   // Decode the JSON
-//   console.log("Got websocket message " + message.data);
-//   window.incoming_data = JSON.parse(message.data);
-//   // Handle errors
-//   if (window.incoming_data.error) {
-//     alert(window.incoming_data.error);
-//     return;
-//   }
-// };
 
 new Vue({
   el: '#app',
